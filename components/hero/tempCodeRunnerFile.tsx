41414c7c53592d81d7be0dd5a448cc8cdce6a41e
suyog3005 <suyoggosavi30@gmail.com>
2026-03-19 03:53:04 +0530

    temp

--- components/hero/tempCodeRunnerFile.tsx
+++
@@ -1,8 +0,0 @@
-textures = [
-    useVideoTexture("/videos/v1.mp4"),
-    useVideoTexture("/videos/v2.mp4"),
-    useVideoTexture("/videos/v3.mp4"),
-    useVideoTexture("/videos/v4.mp4"),
-    useVideoTexture("/videos/v5.mp4"),
-
-  ]
